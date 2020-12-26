224d553325ce0cc4ba44e1fe5392eaefbb0c7322
Lucas Morais <lucasmorais@MBP-de-Lucas.box>
2020-12-26 16:17:36 -0300

    update components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React  from 'react'
 import Maps from './components/maps'
+import './components/styles/app.css'
 
 function App() {
   return (
     <div className="App">
+      <div>
       <Maps />
+      </div> 
+      <div className="stg"><p>Ola</p></div>
+      
     </div>
   );
 }
--- src/components/maps.js
+++ src/components/maps.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
-const MapContainer = () => {
-  
-  const mapStyles = {        
-    height: "100vh",
-    width: "100%"};
-  
-  const defaultCenter = {
-    lat: 41.3851, lng: 2.1734
-  }
-  
+import React, {useState} from 'react'
+import { 
+  GoogleMap, 
+  useLoadScript,
+  Marker,
+  InfoWindow,
+  DirectionsService 
+} from '@react-google-maps/api';
+// importando estilo dark
+import mapStyles from "./mapStyles.js";
+import dados from "./br.json";
+import { useEffect } from 'react';
+
+const libraries = ["places"];
+// 
+const mapContainerStyle = {
+  width: '80vw',
+  height: '80vh',
+}
+// coordenadas para localização central do mapa
+const centro = {
+  lat: -1.298748,
+  lng: -48.479703,
+}
+// opções do mapa => estilo, scroll, zoom .. etc
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+}
+
+
+function Map () {
+  const [data,setData] = useState([])
+  useEffect(() =>{
+    setData(dados)
+  });
+  // configurações do maps
+  const {isLoaded, loadError} = useLoadScript({
+    // Api key fornecida pelo google cloud
+    googleMapsApiKey: "AIzaSyCspyKudsMWQLVXXNUR1boTBAkOdN7m32s",
+    libraries,
+  });
+  // tratamento de erros
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps"
+  // renderização do mapa
+
   return (
-     <LoadScript
-       googleMapsApiKey='YOUR_API_KEY_HERE'>
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={13}
-          center={defaultCenter}
+    <div>
+      <GoogleMap
+      mapContainerStyle={mapContainerStyle} 
+      zoom={15} 
+      center={centro}
+      options={options}>
+
+      {data.map((data) => (
+        <Marker
+        key={data.city}
+        position={{
+          lat: data.lat, lng: data.lng
+        }}
+        icon={{
+          url: "/vacina.svg",
+          scaledSize: new window.google.maps.Size(40,40),
+          origin: new window.google.maps.Point(0,0),
+          anchor: new window.google.maps.Point(20,20),
+        }}
         />
-     </LoadScript>
+      ))}
+      
+      </GoogleMap>
+    </div>
   )
 }
-export default MapContainer;
+export default Map ;
